e017fa08a41abb0a5d98110d34a5554ce7b7c20a
eselbeus <eselbeus@gmail.com>
2019-01-05 01:50:21 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,5 +2,8 @@
 # out the current state.
 def display_board(board)
   print " #{board[0]} | #{board[1]} | #{board[2]} "
+  print "-----------"
   print " #{board[3]} | #{board[4]} | #{board[5]} "
+  print "-----------"
+  print " #{board[6]} | #{board[7]} | #{board[8]} "
 end
